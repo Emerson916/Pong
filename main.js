0c88ee849ab55caec23c9bf6b78b150c4e1f4a4a
Emerson916 <emersons.a296@gmail.com>
2022-05-12 14:44:16 -0300

    Adicionando movimentação do player 1 pelo mouse, refatorando código

--- main.js
+++ main.js
@@ -4,6 +4,23 @@ const contexto = canvas.getContext('2d');
 const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
 
+const openModal = () => document.querySelector('#modal-video').classList.add('active');
+const closeModal = () => document.querySelector('#modal-video').classList.remove('active');
+
+function Reset() {
+    window.location.reload();
+}
+
+const ball = {
+    eixoX: width / 2,
+    eixoY: height / 2,
+    radius: 15,
+    speed: 2,
+    velX: 10,
+    velY: 10,
+    color: "#FF6584"
+}
+
 const user = {
     eixoX: 20,
     eixoY: 200,
@@ -28,14 +45,6 @@ const rede = {
     sizeX: 4,
     sizeY: 15,
     color: "#FF6584",
-}
-
-
-const openModal = () => document.querySelector('#modal-video').classList.add('active');
-const closeModal = () => document.querySelector('#modal-video').classList.remove('active');
-
-function Reset() {
-    window.location.reload();
 }
 
 function random(min, max) {
@@ -67,6 +76,14 @@ function comWinner() {
     }
 }
 
+function drawBall(eixoX, eixoY, radius, color) {
+    contexto.fillStyle = color
+    contexto.beginPath()
+    contexto.arc(eixoX, eixoY, radius, 0, 2 * Math.PI, false)
+    contexto.closePath()
+    contexto.fill()
+}
+
 function drawRect(eixoX, eixoY, sizeX, sizeY, color) {
     contexto.fillStyle = color
     contexto.fillRect(eixoX, eixoY, sizeX, sizeY);
@@ -83,106 +100,6 @@ function drawText(text, eixoX, eixoY, color) {
     contexto.font = "50px fantasy";
     contexto.fillText(text, eixoX, eixoY)
 }
-
-function collision(b, p) {
-    p.top = p.eixoY;
-    p.bottom = p.eixoY + p.height;
-    p.left = p.eixoX;
-    p.right = p.eixoX + p.width;
-
-    b.top = b.y - b.size;
-    b.bottom = b.y + b.size;
-    b.left = b.x - b.size;
-    b.right = b.x + b.size;
-
-    return b.right > p.left && b.top < p.bottom && b.left < p.right && b.bottom > p.top;
-}
-
-function Ball(x, y, velX, velY, color, size) {
-    this.x = x;
-    this.y = y;
-    this.velX = velX;
-    this.velY = velY;
-    this.color = color;
-    this.size = size;
-
-    Ball.prototype.draw = function () {
-        contexto.beginPath();
-        contexto.fillStyle = this.color;
-        contexto.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-        contexto.fill();
-    }
-}
-
-let testBall = new Ball(width / 2, height / 2, 5, 10, '#FF6584', 15);
-
-testBall.x
-testBall.size
-testBall.color
-testBall.draw()
-
-let balls = [testBall];
-
-while (balls.length < 1) {
-    let size = random(10, 20);
-    let ball = new Ball(
-        // ball position always drawn at least one ball width
-        // away from the edge of the canvas, to avoid drawing errors
-        // posição da bola sempre desenhada com pelo menos uma largura de bola
-        // longe da borda da tela, para evitar erros de desenho
-        random(0 + size, width - size),
-        random(0 + size, height - size),
-        random(-7, 7),
-        random(-7, 7),
-        'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')',
-        size
-    );
-
-    balls.push(ball);
-}
-
-// Função movimento e colisão da bola
-
-Ball.prototype.update = function () {
-    if ((this.x + this.size) >= width) {
-        this.velX = -(this.velX);
-        user.score = user.score + 1
-        userWinner()
-    }
-
-    if ((this.x - this.size) <= 0) {
-        this.velX = -(this.velX);
-        com.score = com.score + 1
-        comWinner()
-    }
-
-    if ((this.y + this.size) >= height) {
-        this.velY = -(this.velY);
-    }
-
-    if ((this.y - this.size) <= 0) {
-        this.velY = -(this.velY);
-    }
-
-    this.x += this.velX;
-    this.y += this.velY;
-
-    //IA - Bot
-    let computerLevel = 0.2;
-    com.eixoY += (this.y - (com.eixoY + com.sizeY/2)) * computerLevel;
-
-    let player = (this.x < width / 2) ? user : com;
-
-    if (collision(balls, player)) {
-        let collidePoint = (this.y - (player.eixoY + player.sizeY / 2));
-        collidePoint = collidePoint / (player.sizeY / 2);
-
-        let angleRad = (Math.PI / 4) * collidePoint;
-        let direction = (this.x < width / 2) ? 1 : -1;
-    }
-
-}
-
 
 //função para usar o teclado como controle
 function KeyControls(evt) {
@@ -208,45 +125,122 @@ function KeyControls(evt) {
     }
 }
 
+window.addEventListener("keydown", KeyControls, true);
+
+//função para jogar com o mouse
+function movePaddle(evt) {
+    let rect = canvas.getBoundingClientRect()
+
+    user.eixoY = evt.clientY - rect.top - user.sizeY / 2
+}
+
+canvas.addEventListener("mousemove", movePaddle, true)
+
+function collision(b, p) {
+    b.top = b.eixoY - b.radius;
+    b.bottom = b.eixoY + b.radius;
+    b.left = b.eixoX - b.radius;
+    b.right = b.eixoX + b.radius;
+
+    p.top = p.eixoY;
+    p.bottom = p.eixoY + p.height;
+    p.left = p.eixoX;
+    p.right = p.eixoX + p.width;
+
+    return b.right > p.left && b.bottom > p.top && b.left < p.right && b.top < p.bottom
+}
+
+function update() {
+    ball.eixoX += ball.velX;
+    ball.eixoY += ball.velY;
+
+    //IA - Bot
+    let computerLevel = 0.2;
+    com.eixoY += (ball.eixoY - (com.eixoY + com.sizeY / 2)) * computerLevel;
+
+    // if (ball.eixoY + ball.radius > height || ball.eixoY - ball.radius < 0) {
+    //     ball.velY = -ball.velY;
+    // }
+
+    if ((ball.eixoX + ball.radius) >= width) {
+        ball.velX = -(ball.velX);
+        user.score = user.score + 1
+        userWinner()
+    }
+
+    if ((ball.eixoX - ball.radius) <= 0) {
+        ball.velX = -(ball.velX);
+        com.score = com.score + 1
+        comWinner()
+    }
+
+    if ((ball.eixoY + ball.radius) >= height) {
+        ball.velY = -(ball.velY);
+    }
+
+    if ((ball.eixoY - ball.radius) <= 0) {
+        ball.velY = -(ball.velY);
+    }
+
+    let player = (ball.eixoX < width / 2) ? user : com;
+
+    if (collision(ball, player)) {
+        let collidePoint = ball.eixoY - (player.eixoY + player.sizeY / 2);
+
+        collidePoint = collidePoint / (player.sizeY / 2);
+
+        let angleRad = collidePoint + Math.PI / 4;
+
+        let direction = (ball.eixoX < width / 2) ? 1 : -1;
+
+        ball.velX = direction * ball.speed * Math.cos(angleRad);
+        ball.velY = ball.speed * Math.sin(angleRad);
+
+        ball.speed += 0.1;
+    }
+}
+
 function render() {
     contexto.fillStyle = 'rgba(0, 0, 0, 0.50)';
     contexto.fillRect(0, 0, width, height);
 
-    drawRect(user.eixoX, user.eixoY, user.sizeX, user.sizeY, user.color)
-    drawRect(com.eixoX, com.eixoY, com.sizeX, com.sizeY, com.color)
-    drawRede()
-    drawText(user.score, width / 4, 100, "#FF6584")
-    drawText(com.score, width / 1.3, 100, "#FF6584")
+    drawRect(user.eixoX, user.eixoY, user.sizeX, user.sizeY, user.color);
+    drawRect(com.eixoX, com.eixoY, com.sizeX, com.sizeY, com.color);
+    drawBall(ball.eixoX, ball.eixoY, ball.radius, ball.color);
+    drawRede();
+    drawText(user.score, width / 4, 100, "#FF6584");
+    drawText(com.score, width / 1.35, 100, "#FF6584");
 }
 
-function loop() {
-   
+function game() {
+    update()
     render()
+    // for (let i = 0; i < balls.length; i++) {
+    //     balls[i].draw();
+    //     balls[i].update();
 
-    for (let i = 0; i < balls.length; i++) {
-        balls[i].draw();
-        balls[i].update();
+    //     Ball.prototype.collisionDetect = function () {
+    //         for (let j = 0; j < balls.length; j++) {
+    //             if (!(this === balls[j])) {
+    //                 const dx = this.x - balls[j].x;
+    //                 const dy = this.y - balls[j].y;
+    //                 const distance = Math.sqrt(dx * dx + dy * dy);
 
-        Ball.prototype.collisionDetect = function () {
-            for (let j = 0; j < balls.length; j++) {
-                if (!(this === balls[j])) {
-                    const dx = this.x - balls[j].x;
-                    const dy = this.y - balls[j].y;
-                    const distance = Math.sqrt(dx * dx + dy * dy);
-
-                    if (distance < this.size + balls[j].size) {
-                        balls[j].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')';
-                    }
-                }
-            }
-        }
-        balls[i].collisionDetect();
-    }
-    requestAnimationFrame(loop);
+    //                 if (distance < this.size + balls[j].size) {
+    //                     balls[j].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')';
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     balls[i].collisionDetect();
+    // }
+    // requestAnimationFrame(loop);
 }
 
-window.addEventListener("keydown", KeyControls, true)
+const framePerSecond = 50;
+
 
 function start() {
-    loop();
+    setInterval(game, 1000 / framePerSecond)
+    game();
 }
